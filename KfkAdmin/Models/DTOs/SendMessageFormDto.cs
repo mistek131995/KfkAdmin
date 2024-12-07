805ea8efe4956451037b8048828cfd0bf6325db6
Юрий Кузнецов <omega131995@outlook.com>
2024-12-07 23:19:06 +0700

    Компонент модального окна, вывод групп потребителей

---
+++ KfkAdmin/Models/DTOs/SendMessageFormDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KfkAdmin.Models.DTOs;
+
+public class SendMessageFormDto
+{
+    public string Key { get; set; }
+    [Required(ErrorMessage = "Сообщение обязательно к заполнению")]
+    public string Value { get; set; }
+    public string Headers { get; set; }
+}
