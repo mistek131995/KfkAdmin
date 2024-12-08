40cf5e619e5b2511a9b2f048451735d8cec6466a
Юрий Кузнецов <omega131995@outlook.com>
2024-12-08 21:12:21 +0700

    Регистрация потребителя в DI, удалил старые сервисы

--- KfkAdmin/Extensions/Startup/KafkaExtension.cs
+++ KfkAdmin/Extensions/Startup/KafkaExtension.cs
@@ -9,16 +9,23 @@ public static class KafkaExtension
     public static void AddKafkaExtension(this IServiceCollection services)
     {
         //var host = Environment.GetEnvironmentVariable("KafkaHost");
+        var host = "localhost:9092,localhost:9093";
         
-        services.AddSingleton<IAdminClient>(_ =>
+        services.AddScoped<IAdminClient>(_ =>
         {
             var config = new AdminClientConfig
             {
-                //BootstrapServers = host
-                BootstrapServers = "localhost:9092,localhost:9093"
+                BootstrapServers = host
             };
             return new AdminClientBuilder(config).Build();
         });
+        
+        services.AddScoped<IConsumer<Ignore, string>>(sp => new ConsumerBuilder<Ignore, string>(new ConsumerConfig()
+        {
+            BootstrapServers = host,
+            GroupId = "kfk-admin-group",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        }).Build());
 
         services.AddScoped<IKafkaRepositoryProvider, RepositoryProvider>();
     }
