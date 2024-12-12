ffa5eb36eefe78f5033fa76edeacf709274bcd8e
Юрий Кузнецов <omega131995@outlook.com>
2024-12-12 11:14:48 +0700

    Асинхронный запрос отступов для топиков

--- KfkAdmin/Infrastructure/Repositories/Kafka/TopicRepository.cs
+++ KfkAdmin/Infrastructure/Repositories/Kafka/TopicRepository.cs
@@ -10,17 +10,22 @@ public class TopicRepository(IAdminClient adminClient, IConsumer<string?, string
     public async Task<List<Topic>> GetAllAsync()
     {
         var metadata = await Task.Run(() => adminClient.GetMetadata(TimeSpan.FromSeconds(10)));
-        
-        var topics = metadata.Topics.Select(topic => new Topic()
-        {
-            Name = topic.Topic, 
-            BrokerIds = topic.Partitions.Select(x => x.Leader).Distinct().ToList(),
-            PartitionCount = topic.Partitions.Count, 
-            ReplicationFactor = (short)(topic.Partitions.FirstOrDefault()?.Replicas.Length ?? 0),
-            MessageCount = GetMessageCount(topic)
-        }).ToList();
 
-        return topics;
+        var topics = new List<Topic>();
+        
+        foreach (var topic in metadata.Topics)
+        {
+            topics.Add(new Topic()
+            {
+                Name = topic.Topic, 
+                BrokerIds = topic.Partitions.Select(x => x.Leader).Distinct().ToList(),
+                PartitionCount = topic.Partitions.Count, 
+                ReplicationFactor = (short)(topic.Partitions.FirstOrDefault()?.Replicas.Length ?? 0),
+                MessageCount = await GetMessageCount(topic)
+            });
+        }
+        
+        return topics.ToList();
     }
 
     public async Task<List<Topic>> GetByBrokerIdAsync(int brokerId)
@@ -37,7 +42,7 @@ public class TopicRepository(IAdminClient adminClient, IConsumer<string?, string
                 BrokerIds = topic.Partitions.Select(x => x.Leader).Distinct().ToList(),
                 PartitionCount = topic.Partitions.Count, 
                 ReplicationFactor = (short)(topic.Partitions.FirstOrDefault()?.Replicas.Length ?? 0),
-                MessageCount = GetMessageCount(topic)
+                MessageCount = await GetMessageCount(topic)
             });
         }
 
@@ -58,7 +63,7 @@ public class TopicRepository(IAdminClient adminClient, IConsumer<string?, string
             PartitionCount = topic.Partitions.Count,
             ReplicationFactor = (short)(topic.Partitions.FirstOrDefault()?.Replicas.Length ?? 0),
             BrokerIds = topic.Partitions.Select(x => x.Leader).Distinct().ToList(),
-            MessageCount = GetMessageCount(topic)
+            MessageCount = await GetMessageCount(topic)
         };
     }
 
@@ -77,13 +82,13 @@ public class TopicRepository(IAdminClient adminClient, IConsumer<string?, string
     public async Task DeleteAsync(string name) => await adminClient.DeleteTopicsAsync([name]);
     public async Task DeleteAsync(List<string> names) => await adminClient.DeleteTopicsAsync(names);
     
-    private long GetMessageCount(TopicMetadata topicMetadata)
+    private async Task<long> GetMessageCount(TopicMetadata topicMetadata)
     {
         long count = 0;
 
         foreach (var partition in topicMetadata.Partitions)
         {
-            var offset = consumer.QueryWatermarkOffsets(new TopicPartition(topicMetadata.Topic, partition.PartitionId), TimeSpan.FromSeconds(10));
+            var offset = await Task.Run(() => consumer.QueryWatermarkOffsets(new TopicPartition(topicMetadata.Topic, partition.PartitionId), TimeSpan.FromSeconds(10)));
             
             if(offset is not null)
                 count += offset.High.Value - offset.Low.Value;
